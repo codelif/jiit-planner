20a8985493783d53e51b29362e2323eb702ba4f4
Harsh Sharma <harsh@codelif.in>
2024-07-27 15:57:59 +0530

    feat: update ui

--- jiit_planner/__init__.py
+++ jiit_planner/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 from jiit_planner.routes import router
 
 PROG = "jiit-planner"
-app = Flask(PROG, static_folder="jiit_planner/static", template_folder="jiit_planner/templates")
+app = Flask(PROG, static_folder="jiit_planner/assets", template_folder="jiit_planner/templates")
 app.register_blueprint(router)
--- jiit_planner/routes.py
+++ jiit_planner/routes.py
@@ -3,6 +3,8 @@ from jiit_tt_parser.parser import parse_events
 from jiit_tt_parser.utils import load_worksheet
 from jiit_tt_parser.utils.preprocessing import cache_tt_xls, cache_fac, os
 from jiit_tt_parser.utils.cache import get_cache_file
+from typing import List
+from jiit_tt_parser.parser.parse_events import Event
 
 router = Blueprint("router", "jiit-planner", template_folder="jiit_planner/templates")
 
@@ -16,8 +18,21 @@ def home():
 def planner(batch):
     evs = filter_events(batch)
 
-    return render_template("planner.html", events=evs)
+    return render_template("index.html", events=evs)
 
+
+
+def jsonify_events(events: List[Event]):
+    {"batches": ["B5"],
+     "type": "L",
+     "classroom": "CL03",
+     "subject": "Computer Fundamentals",
+     "subject-code": "CI111",
+     }
+
+    for ev in events:
+        ev.eventcode
+        
 
 def filter_events(batch: str):
     sheet, r, c = load_worksheet(get_cache_file("sem1.xlsx"))

e21f9770feeef8ab9d04a70c1f4f5794e9d0b419
Srijan Baniyal <john.rambo.9901@gmail.com>
2024-07-30 20:01:04 +0530

    FrontEnd: Remaining with the Footer , Till now done with Cards , DropDown and Navigation Menu and Will complete it soon....

--- jiit_planner/assets/js/main.js
+++ jiit_planner/assets/js/main.js
@@ -1,57 +1,66 @@
+var animateButton = function (e) {
+  e.preventDefault;
+  e.target.classList.remove("animate");
+  e.target.classList.add("animate");
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener("click", animateButton, false);
+}
 const hamburger = document.querySelector(".hamburger");
-      const navLinks = document.querySelector(".nav-links");
-      const links = document.querySelectorAll(".nav-links li");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+  hamburger.classList.toggle("toggle");
+});
 
-      hamburger.addEventListener("click", () => {
-        //Animate Links
-        navLinks.classList.toggle("open");
-        links.forEach((link) => {
-          link.classList.toggle("fade");
-        });
+let params = new URL(document.location.toString()).searchParams;
+let batch = params.get("b");
+let day = params.get("day");
 
-        //Hamburger Animation
-        hamburger.classList.toggle("toggle");
-      });
+if (batch == null) {
+  let last_batch = get_last_batch();
+  if (last_batch != null) {
+    let loc = `/batch?b=${last_batch}`;
+    if (day != null) {
+      loc += `&day=${day}`;
+    }
+    location.href = loc;
+  }
+} else {
+  set_last_batch(batch);
+}
 
-      let params = new URL(document.location.toString()).searchParams;
-      let batch = params.get("b");
-      let day = params.get("day");
+function batch_change() {
+  let b = document.getElementById("batch_input");
+  let d = document.getElementById("day");
+  if (b.value != "None") {
+    set_last_batch(b.value);
+  }
+  let loc = `/batch?b=${b.value}&day=${d.value}`;
+  location.href = loc;
+}
+function today() {
+  let b = document.getElementById("batch_input");
+  if (b.value != "None") {
+    set_last_batch(b.value);
+  }
+  let loc = `/batch?b=${b.value}`;
+  location.href = loc;
+}
 
-      if (batch == null) {
-        let last_batch = get_last_batch();
-        if (last_batch != null) {
-          let loc = `/batch?b=${last_batch}`;
-          if (day != null) {
-            loc += `&day=${day}`;
-          }
-          location.href = loc;
-        }
-      } else {
-        set_last_batch(batch);
-      }
+function get_last_batch() {
+  return localStorage.getItem("batch");
+}
 
-      function batch_change() {
-        let b = document.getElementById("batch_input");
-        let d = document.getElementById("day");
-        if (b.value != "None") {
-          set_last_batch(b.value);
-        }
-        let loc = `/batch?b=${b.value}&day=${d.value}`;
-        location.href = loc;
-      }
-      function today() {
-        let b = document.getElementById("batch_input");
-        if (b.value != "None") {
-          set_last_batch(b.value);
-        }
-        let loc = `/batch?b=${b.value}`;
-        location.href = loc;
-      }
-
-      function get_last_batch() {
-        return localStorage.getItem("batch");
-      }
-
-      function set_last_batch(batch) {
-        localStorage.setItem("batch", batch);
-      }
+function set_last_batch(batch) {
+  localStorage.setItem("batch", batch);
+}
